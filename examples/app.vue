37f3d461e345b3d59397e72ecefe49227b1a7923
roundlee <round20@163.com>
2022-09-05 18:20:38 +0800

    fix: 修改ui名称

--- examples/app.vue
+++ examples/app.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <!-- 启动测试 -->
+        启动测试
         <Button />
     </div>
 </template>
 <script lang="ts" setup>
-import  { Button }  from 'round-ui'
+import  { Button }  from 'roundlees-ui'
 </script>
